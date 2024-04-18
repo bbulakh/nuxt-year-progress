533c2744f352048af424e88cf791fc8c063e91a9
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-18 17:13:06 +0300

    move app.vue to root directory

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import MainLayout from './src/layouts/MainLayout.vue';
+  import YearProgress from './src/components/YearProgressBar.vue';
+</script>
+
+<template>
+  <MainLayout>
+    <YearProgress/>
+  </MainLayout>
+</template>
