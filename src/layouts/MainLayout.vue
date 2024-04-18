4eda76d5c67621e3f7b869ecac7e0fd9abb0715b
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-18 17:13:47 +0300

    add main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,3 +1,25 @@
+<script setup>
+let currentYear = new Date().getFullYear();
+</script>
+
 <template>
-    <slot/>
+  <main>
+    <div class="content text-red-500">
+      <h1 class="content__heading">{{ currentYear }} Year Progress Bar</h1>
+      <slot />
+      <p>{{ currentYear }} is</p>
+    </div>
+  </main>
 </template>
+
+<style scoped>
+.content {
+  width: 50%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+</style>
