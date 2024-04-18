75fc48f80e319e009dbc6434f346ef198539044c
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-18 17:12:12 +0300

    comment electron section

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
-})
+  modules: ["@nuxtjs/tailwindcss"],
+  // electron: {
+  //   build: [
+  //     {
+  //       // Main-Process entry file of the Electron App.
+  //       entry: 'electron/main.ts',
+  //     },
+  //   ],
+  // },
+});
