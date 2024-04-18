24d30ecc670a02315f33aa56ba59b481a87a40cb
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-18 17:13:55 +0300

    add vite config

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import electron from 'vite-plugin-electron/simple'
+
+export default {
+  plugins: [
+    electron({
+      main: {
+        // Shortcut of `build.lib.entry`
+        entry: 'electron/main.ts',
+      },
+      preload: {
+        // Shortcut of `build.rollupOptions.input`
+        input: 'electron/preload.ts',
+      },
+      // Optional: Use Node.js API in the Renderer process
+      renderer: {},
+    }),
+  ],
+}
