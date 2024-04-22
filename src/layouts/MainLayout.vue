98fbfc2b99c86b008d097da2952d7200542eb34d
Bogdan Bulakh <95144713+bbulakh@users.noreply.github.com>
2024-04-22 18:01:19 +0300

    add gradient background

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,22 +4,51 @@ let currentYear = new Date().getFullYear();
 
 <template>
   <main>
-    <div class="content text-red-500">
-      <h1 class="content__heading">{{ currentYear }} Year Progress Bar</h1>
+    <img
+      class="absolute w-full h-full pointer-events-none opacity-15"
+      src="/public/Noise.png"
+      alt=""
+    />
+    <div class="content px-5">
+      <h1 class="text-5xl font-bold text-white text-center mb-5 z-10">
+        {{ currentYear }} PROGRESS
+      </h1>
       <slot />
-      <p>{{ currentYear }} is</p>
     </div>
   </main>
 </template>
 
 <style scoped>
 .content {
-  width: 50%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
+}
+
+main {
+  background: hsla(191, 75%, 60%, 1);
+
+  background: linear-gradient(
+    90deg,
+    hsla(191, 75%, 60%, 1) 0%,
+    hsla(248, 87%, 36%, 1) 100%
+  );
+
+  background: -moz-linear-gradient(
+    90deg,
+    hsla(191, 75%, 60%, 1) 0%,
+    hsla(248, 87%, 36%, 1) 100%
+  );
+
+  background: -webkit-linear-gradient(
+    90deg,
+    hsla(191, 75%, 60%, 1) 0%,
+    hsla(248, 87%, 36%, 1) 100%
+  );
+
+  filter: progid: DXImageTransform.Microsoft.gradient( startColorstr="#4DC9E6", endColorstr="#210CAE", GradientType=1 );
 }
 </style>
